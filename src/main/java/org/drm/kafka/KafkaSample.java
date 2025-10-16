52fa1e7ef1da4f1fe2e6e3f66ab115dbbbe92ef8
viktar-b <drmhere@yandex.by>
2025-10-16 12:47:23 +0300

    #add KAFKA sample

---
+++ src/main/java/org/drm/kafka/KafkaSample.java
@@ -0,0 +1,21 @@
+package org.drm.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaSample {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        producer.send(new ProducerRecord<String, String>("visitors", "visitor1"));
+
+        producer.close();
+    }
+}
