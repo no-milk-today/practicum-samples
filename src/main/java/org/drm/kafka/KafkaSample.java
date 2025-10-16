2b1a810c2b23888f61f3fde3b1bcba02ec657f42
viktar-b <drmhere@yandex.by>
2025-10-16 12:54:42 +0300

    #kafka partitions

--- src/main/java/org/drm/kafka/KafkaSample.java
+++ src/main/java/org/drm/kafka/KafkaSample.java
@@ -6,6 +6,18 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 
+/**
+Партиции в Apache Kafka — это не копии, а разные части (части) одного топика,
+ и каждая партиция содержит свой уникальный набор сообщений.
+
+* Топик — это как большая очередь сообщений.
+
+* Чтобы ускорить обработку и масштабировать систему, очередь разбивается на несколько стопок — партиций.
+
+* Каждая партиция — это своя собственная линейная последовательность сообщений, не пересекающаяся с другими партициями топика.
+
+* Сообщения в топике распределяются между партициями по ключу или по алгоритму равномерного распределения, так что разные сообщения попадают в разные партиции.
+ **/
 public class KafkaSample {
     public static void main(String[] args) {
         Properties props = new Properties();
@@ -14,7 +26,13 @@ public class KafkaSample {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
-        producer.send(new ProducerRecord<String, String>("visitors", "visitor1"));
+        producer.send(new ProducerRecord<String, String>("visitors", "visitor1")); // простой send без разбиения на партиции
+
+        for (int i=0; i<30; i++) { // Партиционирование сообщений
+            producer.send(new ProducerRecord<String, String>("visitors", "key1", "visitor" + i + "_1"));
+            producer.send(new ProducerRecord<String, String>("visitors", "key2", "visitor" + i + "_2"));
+            producer.send(new ProducerRecord<String, String>("visitors", "key3", "visitor" + i + "_3"));
+        }
 
         producer.close();
     }
